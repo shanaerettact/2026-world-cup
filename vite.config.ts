bb02a6da973ddbc6d149c97d3257d697704a710b
johnny <johnny111@gmai.com>
2026-05-09 23:17:29 +0800

    feat: 移除vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 import { fileURLToPath } from 'url'
 
 export default defineConfig({
   base: './',
-  plugins: [vue(), tailwindcss(), viteSingleFile()],
+  plugins: [vue(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
